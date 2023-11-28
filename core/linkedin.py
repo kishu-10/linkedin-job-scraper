8b91acfa342412fbc90430e234d307ead4dfb731
Kishu Maharjan <kishu@Kishus-MacBook-Pro.local>
2023-11-28 09:23:33 +0545

    feat: add beautifulsoup

--- core/linkedin.py
+++ core/linkedin.py
@@ -1,6 +1,7 @@
 from decouple import config
 import requests
 from fastapi.responses import RedirectResponse
+from bs4 import BeautifulSoup
 
 
 class LinkedIn:
@@ -8,6 +9,7 @@ class LinkedIn:
     client_secret = config("CLIENT_SECRET")
     redirect_uri = config("REDIRECT_URI")
     grant_type = "authorization_code"
+    linkedin_url = config("LINKEDIN_URL")
 
     def authorization(self):
         url = "https://www.linkedin.com/oauth/v2/authorization"
@@ -38,3 +40,42 @@ class LinkedIn:
         headers = {"Authorization": f"Bearer {access_token}"}
         response = requests.post(url, headers=headers)
         return response.json()["message"]
+
+    def scrape_jobs(self, location="Nepal"):
+        _url = f"{self.linkedin_url}jobs/search"
+        _params = {
+            "keywords": "",
+            "location": location,
+            "geoId": "",
+            "trk": "public_jobs_jobs-search-bar_search-submit",
+            "position": 1,
+            "pageNum": 0,
+        }
+        response = requests.get(_url, params=_params)
+        soup = BeautifulSoup(response.content, "html.parser")
+        job_cards = soup.find_all("div", class_="base-search-card__info")
+        job_list = []
+
+        for job_card in job_cards:
+            job_dict = {}
+
+            title = job_card.find("h3", class_="base-search-card__title")
+            job_dict["title"] = title.text.strip() if title else None
+
+            company = job_card.find("h4", class_="base-search-card__subtitle")
+            job_dict["company"] = company.text.strip() if company else None
+
+            job_location = job_card.find("span", class_="job-search-card__location")
+            job_dict["location"] = job_location.text.strip() if job_location else None
+
+            benefits = job_card.find("span", class_="result-benefits__text")
+            job_dict["benefits"] = benefits.text.strip() if benefits else None
+
+            published_date = job_card.find("time", class_="job-search-card__listdate")
+            job_dict["published_date"] = (
+                published_date.text.strip() if published_date else None
+            )
+
+            job_list.append(job_dict)
+
+        return job_list
