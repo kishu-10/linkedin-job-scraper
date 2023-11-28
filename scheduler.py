027e2ee6ab8cd39117d38daf9ea3cac2c4768bd8
Kishu Maharjan <kishu@Kishus-MacBook-Pro.local>
2023-11-28 11:43:36 +0545

    fix: scheduler task

--- scheduler.py
+++ scheduler.py
@@ -16,7 +16,7 @@ def schedule_job_task():
 
 default_args = {
     "owner": "airflow",
-    "start_date": datetime(2023, 1, 1),
+    "start_date": datetime(2023, 11, 29),
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
