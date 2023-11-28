89a2623a768be582988635c1b862abd8b1e8100e
Kishu Maharjan <kishu@Kishus-MacBook-Pro.local>
2023-11-28 10:47:02 +0545

    fix: save to csv file using pd

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from core.api import api
+from core.api import api_app
 from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-]
-app.add_middleware(SessionMiddleware, secret_key="Foo")
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-app.mount("/api", api)
+app.add_middleware(SessionMiddleware, secret_key="Foo")
+
+app.mount("/api", api_app)
 
 
 @app.get("/")
